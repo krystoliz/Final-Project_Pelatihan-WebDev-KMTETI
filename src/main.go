592197ed215a71ca30ae33c446b1da29bfabff45
krystoliz <109867611+krystoliz@users.noreply.github.com>
2024-09-30 15:30:36 +0700

    <added: basic api handler>

--- src/main.go
+++ src/main.go
@@ -4,12 +4,20 @@ import ("fmt"
 		"net/http")
 func main(){
 	s := &http.Server{
-		Addr: ":8000",
+		Addr: ":8080",
 	}
-	
-	fmt.Println("HTTP Server is running on port 8000")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		w.Write([]byte("Hello from server"))
+	})
+	http.HandleFunc("/api/user", func(w http.ResponseWriter, r *http.Request){
+		w.Write([]byte("Hello from /api/user"))
+	})
+	http.HandleFunc("/api/product", func(w http.ResponseWriter, r *http.Request){
+		w.Write([]byte("Hello from /api/product"))
+	})
+	fmt.Println("HTTP Server is running on port 8080")
 	err := s.ListenAndServe()
-	if err != nil {
+	if err != nil { //error handling
 		fmt.Println(err.Error())
 	}
 	
