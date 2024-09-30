64625c1c330d2684567e38f017a1970ea8952f1c
krystoliz <109867611+krystoliz@users.noreply.github.com>
2024-09-30 15:37:56 +0700

    <refactored: main function --> handler functions>

--- src/main.go
+++ src/main.go
@@ -2,19 +2,29 @@ package main
 
 import ("fmt"
 		"net/http")
+
+func userHandler(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Hello from /api/users"))
+}
+
+func productHandler(w http.ResponseWriter, r *http.Request){
+    w.Write([]byte("Hello from /api/product"))
+}
+
+ 
 func main(){
 	s := &http.Server{
 		Addr: ":8080",
 	}
+
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
 		w.Write([]byte("Hello from server"))
 	})
-	http.HandleFunc("/api/user", func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("Hello from /api/user"))
-	})
-	http.HandleFunc("/api/product", func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("Hello from /api/product"))
-	})
+
+	http.HandleFunc("/api/user", userHandler)
+	http.HandleFunc("/api/product", productHandler)
+
 	fmt.Println("HTTP Server is running on port 8080")
 	err := s.ListenAndServe()
 	if err != nil { //error handling
