59def58eeb08097c5ffff51a9f3d7abf93a66521
krystoliz <109867611+krystoliz@users.noreply.github.com>
2024-10-01 10:30:10 +0700

    <added: product struct>

--- src/handler/product.go
+++ src/handler/product.go
@@ -2,6 +2,13 @@ package handler
 
 import "net/http"
 
+type Product struct{
+	Id int// if its lowercase its detected as local variable
+	Name string
+	Price int
+	Stock uint
+}
+
 func ProductHandler(w http.ResponseWriter, r *http.Request){
 	if r.Method != "GET" {
 		w.WriteHeader(http.StatusMethodNotAllowed) 
