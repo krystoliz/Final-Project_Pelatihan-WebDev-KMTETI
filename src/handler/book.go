f3ff0d8621e8b5f1a42734de25998dc217ad8084
krystoliz <109867611+krystoliz@users.noreply.github.com>
2024-11-03 21:50:28 +0700

    <added: fields based on the project requirements>

--- src/handler/book.go
+++ src/handler/book.go
@@ -1,51 +1,81 @@
 package handler
 
 import (
+	"context"
 	"encoding/json"
-	
+	"fmt"
 	"net/http"
+
+	"github.com/krystoliz/Final-Project_Pelatihan-WebDev-KMTETI/src/db"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 type Book struct{
-	Id int `json:"id"`// if its lowercase its detected as local variable
+	// if its lowercase its detected as local variable
 	Title string `json:"title"`
 	Author string `json:"author"`
-	Pages int `json:"pages"`
-	Year uint `json:"year"`
+	Stock int `json:"stock"`
+	Year_released uint `json:"year_released"`
+	Price uint `json:"price"`
 }
 
 var BookList []*Book = []*Book{
 	 &Book{
-		Id: 1,
+		
         Title: "Unf/Air",
         Author: "ARTMS",
-        Pages: 120,
-		Year: 2024,
+        Stock: 120,
+		Year_released: 2024,
+		Price: 100000,
 	},
 
 	&Book{
-		Id: 2,
+		
         Title: "Cotton Candy",
         Author: "Loossemble",
-        Pages: 120,
-		Year: 2024,
+        Stock: 120,
+		Year_released: 2024,
+		Price: 100000,
 	},
 }
 
 func BookHandler(w http.ResponseWriter, r *http.Request){
 	switch r.Method {
 		case "GET":
+			db, err := db.DBConnection()
+			if err != nil{
+				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+				return
+			}
+
+			coll := db.MongoDB.Collection("buku")
+			cur, err := coll.Find(context.TODO(), bson.D{})
+			if err != nil{
+				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+				return
+			}
+			var prod BookModel
+			var bookList []*BookModel
+
+			for cur.Next(context.TODO()){
+				cur.Decode(&prod)
+				fmt.Println(prod)
+				bookList = append(bookList, &prod)
+			}
+
             w.Header().Add("Content-Type", "application/json")
-            json.NewEncoder(w).Encode(BookList)
+            json.NewEncoder(w).Encode(bookList)
             return
+
+
 
 		case "POST":
 			var b Book 
-			json.NewDecoder(r.Body).Decode(&b)
-			BookList = append(BookList, &b)
-			w.WriteHeader(http.StatusCreated)
-			w.Write([]byte("book added successfully"))
-			return
+			err := json.NewDecoder(r.Body).Decode(&b)
+			if err != nil{
+				http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+				return
+			}
 		
 		case "PUT":
 
@@ -58,3 +88,17 @@ func BookHandler(w http.ResponseWriter, r *http.Request){
 	}
 
 }
+
+type BookModel struct{
+	ID string `bson:"_id,emitonempty"`
+	Title string `bson:"title"`
+	Author string `bson:"author"`
+	Stock int `bson:"stock"`
+	Year_released int `bson:"year_released"`
+	Price int `bson:"price"`
+}
+
+func NewProductHandler(w http.ResponseWriter, r *http.Request){
+	
+  w.Write([]byte("connection success"))
+}	
