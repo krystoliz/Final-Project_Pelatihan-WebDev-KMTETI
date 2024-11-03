a9584cb162ebac3c5e636446427267e981a6b9f9
krystoliz <109867611+krystoliz@users.noreply.github.com>
2024-11-03 21:49:28 +0700

    <refactored: http usage>

--- src/main.go
+++ src/main.go
@@ -1,34 +1,38 @@
 package main
 
-import ("fmt"
-		"net/http"
-		"github.com/krystoliz/Final-Project_Pelatihan-WebDev-KMTETI/src/handler")
+import (
+	"fmt"
+	"net/http"
 
-func userHandler(w http.ResponseWriter, r *http.Request){
+	/* "github.com/krystoliz/Final-Project_Pelatihan-WebDev-KMTETI/src/db" */
+	"github.com/krystoliz/Final-Project_Pelatihan-WebDev-KMTETI/src/handler"
+)
+
+func userHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from /api/users"))
 }
 
+func main() {
 
-
- 
-func main(){
-	s := &http.Server{
-		Addr: ":8080",
-	}
+	h := http.NewServeMux()
 	
+	s := &http.Server{
+		Addr:    ":8080",
+		Handler: h,
+	}
 
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+	h.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello from server"))
 	})
 
-	http.HandleFunc("/api/user", userHandler)
-	http.HandleFunc("/api/books", handler.BookHandler) //if p is lowercase it wont be accessible to this file. If p is uppercase it will be accessible.
+	h.HandleFunc("/api/user", userHandler)
+	h.HandleFunc("/api/books", handler.BookHandler) //if p is lowercase it wont be accessible to this file. If p is uppercase it will be accessible.
+	h.HandleFunc("/api/test-db", handler.NewProductHandler)
 
 	fmt.Println("HTTP Server is running on port 8080")
 	err := s.ListenAndServe()
 	if err != nil { //error handling
 		fmt.Println(err.Error())
 	}
-	
+
 }
